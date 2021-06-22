a9225380f02f82507d2cecd83f47386b1c6d5f9c
Lucas Sousa <joselmdbsousa@ua.pt>
2021-06-22 16:08:09 +0100

    post now goes to engine, rather be this way than not being working at all

--- src/environment.js
+++ src/environment.js
@@ -1,5 +1,11 @@
 /*
  * CLIENT ENDPOINT
-*/
+ */
 const CLIENT_HOST = "https://cito-client.herokuapp.com";
 export const CLIENT_ENDPOINT = CLIENT_HOST + "/clientApi/";
+
+/*
+ * ENGINE ENDPOINT
+ */
+const ENGINE_HOST = "https://cito-engine.herokuapp.com";
+export const CLIENT_ENDPOINT_ENGINE = ENGINE_HOST + "/clientApi/";
